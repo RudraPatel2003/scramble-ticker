32aacfafc90c369e244617ade87838b3dd4bcc62
Rudra Patel <rudra.patel@sentry.io>
2026-08-13 14:35:34 -0700

    chore: WIP

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { App } from "./components/app/app.tsx";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+
+const reactRoot = ReactDOM.createRoot(rootElement);
+
+reactRoot.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
 
 // Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  // eslint-disable-next-line no-console
+  console.log(message);
+});
